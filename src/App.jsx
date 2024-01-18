e168f2b655875e795808dfd0991d7f139fc9f0b4
hexadivine <hexadivine@gmail.com>
2024-01-19 01:01:01 +0530

    added search category and country feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import './App.css'
-import CategoriesSidebar from './components/Sidebar/CategoriesSidebar';
-import CountriesSidebar from './components/Sidebar/CountriesSidebar';
-
+import "./App.css";
+import CategoriesSidebar from "./components/Sidebar/CategoriesSidebar";
+import CountriesSidebar from "./components/Sidebar/CountriesSidebar";
+import { useState } from "react";
 function App() {
+
+    const [selectedCategory, setSelectedCategory] = useState();
+    const [selectedCountry, setSelectedCountry] = useState();
+
     return (
-        <div className='grid grid-flow-col grid-cols-[200px_1fr_200px]'>
-            <CategoriesSidebar />
-            <div>
-               
-            </div>
-            <CountriesSidebar />
+        <div className="grid grid-flow-col grid-cols-[200px_1fr_200px]">
+            <CategoriesSidebar category={selectedCategory} selectCategory={setSelectedCategory} />
+            <div className="bg-gradient-to-r from-[#390b26]  via-[#020618] via-[#020618] via-[#020618]  to-[#390b26]"></div>
+            <CountriesSidebar country={selectedCountry} selectCountry={setSelectedCountry} />
         </div>
     );
 }
