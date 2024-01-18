c6f79052fee1f369e809106e1236cf6f6118bbfd
hexadivine <hexadivine@gmail.com>
2024-01-18 18:27:54 +0530

    added sidebars for catagory and country selection

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+import CategoriesSidebar from './components/Sidebar/CategoriesSidebar';
+import CountriesSidebar from './components/Sidebar/CountriesSidebar';
 
 function App() {
     return (
-        <>
-            
-        </>
+        <div className='grid grid-flow-col grid-cols-[200px_1fr_200px]'>
+            <CategoriesSidebar />
+            <div>
+               
+            </div>
+            <CountriesSidebar />
+        </div>
     );
 }
 
