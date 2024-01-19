28d1fdd9c0bd0d1a6f86d0168e401790cfc3a187
hexadivine <hexadivine@gmail.com>
2024-01-19 12:25:12 +0530

    added 3 divs for logo search and weather

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <div className="flex h-[115px] justify-center items-center gap-[20px] ">
+            <div className="flex w-[150px] h-[40px] justify-center items-center bg-[#ffffff30]  text-center rounded-full">
+                News++
+            </div>
+            <div className="">
+                <input type="text" className="h-[40px] rounded-full" />
+            </div>
+            <div className="flex  justify-center items-center w-[150px] h-[40px] bg-[#ffffff30]  gap-[10px] rounded-full">
+                <img
+                    src="/cloud-sun.png"
+                    alt="cloud"
+                    className="h-[25px] w-[25px] bg-[#ffffff30] "
+                />
+                <div>23°c</div>
+            </div>
+        </div>
+    );
 }
 
-export default Navbar
+export default Navbar;
