ccee279a5d8a287148907c99e6e27bfe8b8fac50
hexadivine <hexadivine@gmail.com>
2024-01-19 12:24:07 +0530

    changed bg from plain to gredient

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 import CategoriesSidebar from "./components/Sidebar/CategoriesSidebar";
 import CountriesSidebar from "./components/Sidebar/CountriesSidebar";
 import { useState } from "react";
+
 function App() {
 
     const [selectedCategory, setSelectedCategory] = useState();
@@ -10,7 +12,9 @@ function App() {
     return (
         <div className="grid grid-flow-col grid-cols-[200px_1fr_200px]">
             <CategoriesSidebar category={selectedCategory} selectCategory={setSelectedCategory} />
-            <div className="bg-gradient-to-r from-[#390b26]  via-[#020618] via-[#020618] via-[#020618]  to-[#390b26]"></div>
+            <div className="bg-gradient-to-r from-[#390b26]  via-[#020618] via-[#020618] via-[#020618]  to-[#390b26]">
+                <Navbar />
+            </div>
             <CountriesSidebar country={selectedCountry} selectCountry={setSelectedCountry} />
         </div>
     );
