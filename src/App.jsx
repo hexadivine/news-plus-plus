1dec4af383641757832054b40445c5ce59e8846e
hexadivine <hexadivine@gmail.com>
2024-01-19 16:26:40 +0530

    added toggle functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,49 @@ import CountriesSidebar from "./components/Sidebar/CountriesSidebar";
 import { useState } from "react";
 
 function App() {
-
     const [selectedCategory, setSelectedCategory] = useState();
     const [selectedCountry, setSelectedCountry] = useState();
 
+    const [toggleLeftSidebar, setToggleLeftSidebar] = useState(true);
+    const [toggleRightSidebar, setToggleRightSidebar] = useState(true);
+
     return (
-        <div className="grid grid-flow-col grid-cols-[200px_1fr_200px]">
-            <CategoriesSidebar category={selectedCategory} selectCategory={setSelectedCategory} />
-            <div className="bg-gradient-to-r from-[#390b26]  via-[#020618] via-[#020618] via-[#020618]  to-[#390b26]">
-                <Navbar />
+        <div
+            className={`grid grid-flow-col ${
+                toggleLeftSidebar && toggleRightSidebar
+                    ? "grid-cols-[200px_1fr_200px]"
+                    : toggleLeftSidebar
+                      ? "grid-cols-[200px_1fr]"
+                      : toggleRightSidebar
+                        ? "grid-cols-[1fr_200px]"
+                        : ""
+            }`}
+        >
+            {toggleLeftSidebar ? (
+                <CategoriesSidebar
+                    category={selectedCategory}
+                    selectCategory={setSelectedCategory}
+                />
+            ) : (
+                ""
+            )}
+
+            <div className="h-screen bg-gradient-to-r from-[#390b26]  via-[#020618] via-[#020618] via-[#020618]  to-[#390b26]">
+                <Navbar
+                    toggleLeftSidebar={toggleLeftSidebar}
+                    setToggleLeftSidebar={setToggleLeftSidebar}
+                    toggleRightSidebar={toggleRightSidebar}
+                    setToggleRightSidebar={setToggleRightSidebar}
+                />
             </div>
-            <CountriesSidebar country={selectedCountry} selectCountry={setSelectedCountry} />
+            {toggleRightSidebar ? (
+                <CountriesSidebar
+                    country={selectedCountry}
+                    selectCountry={setSelectedCountry}
+                />
+            ) : (
+                ""
+            )}
         </div>
     );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,58 @@
 import React from "react";
+import { RiMenuFoldFill } from "react-icons/ri";
+import { RiMenuUnfoldFill } from "react-icons/ri";
 
-function Navbar() {
+function Navbar({
+    toggleLeftSidebar,
+    setToggleLeftSidebar,
+    toggleRightSidebar,
+    setToggleRightSidebar,
+}) {
     return (
         <div className="flex h-[115px] justify-center items-center gap-[20px] ">
-            <div className="flex w-[150px] h-[40px] justify-center items-center bg-[#ffffff30]  text-center rounded-full">
+            <div className="bg-[#58585830] rounded-full p-[10px]">
+                {toggleLeftSidebar ? (
+                    <RiMenuFoldFill
+                        className="size-[20px]"
+                        onClick={() => setToggleLeftSidebar((prev) => !prev)}
+                    />
+                ) : (
+                    <RiMenuUnfoldFill
+                        className="size-[20px]"
+                        onClick={() => setToggleLeftSidebar((prev) => !prev)}
+                    />
+                )}
+            </div>
+            <div className="flex w-[150px] h-[40px] justify-center items-center bg-[#58585830]  text-center rounded-full">
                 News++
             </div>
             <div className="">
-                <input type="text" className="h-[40px] rounded-full" />
+                <input
+                    type="text"
+                    className="bg-[#58585830] h-[40px] rounded-full text-center focus:placeholder-transparent focus:outline-none"
+                    placeholder="Search"
+                />
             </div>
-            <div className="flex  justify-center items-center w-[150px] h-[40px] bg-[#ffffff30]  gap-[10px] rounded-full">
+            <div className="flex  justify-center items-center w-[150px] h-[40px] bg-[#58585830]  gap-[10px] rounded-full">
                 <img
                     src="/cloud-sun.png"
                     alt="cloud"
-                    className="h-[25px] w-[25px] bg-[#ffffff30] "
+                    className="h-[25px] w-[25px]"
                 />
                 <div>23°c</div>
+            </div>
+            <div className="bg-[#58585830] rounded-full p-[10px]">
+                {!toggleRightSidebar ? (
+                    <RiMenuFoldFill
+                        className="size-[20px]"
+                        onClick={() => setToggleRightSidebar((prev) => !prev)}
+                    />
+                ) : (
+                    <RiMenuUnfoldFill
+                        className="size-[20px]"
+                        onClick={() => setToggleRightSidebar((prev) => !prev)}
+                    />
+                )}
             </div>
         </div>
     );
