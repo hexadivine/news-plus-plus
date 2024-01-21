39ae232011f0baea1523294ab11f5641206f6604
hexadivine <hexadivine@gmail.com>
2024-01-21 23:39:09 +0530

    reconstucted fetch news module for easy use

--- src/modules/fetchNews.modules.js
+++ src/modules/fetchNews.modules.js
@@ -1,29 +1,47 @@
-import { API_URLS_EVERYTHING_ENDPOINT } from "./../config/config";
+import { API_KEYS } from "./../config/env";
 
-export async function fetchNews(newsDict, setNewsDict, setNewsDictBackup) {
-    const URLS = API_URLS_EVERYTHING_ENDPOINT;
+export async function fetchNews(
+    URL = "",
+    API_BASE_URL = "https://newsdata.io/",
+    ENDPOINT = "api/1/news?",
+    PARAMS = "q=Elon"
+) {
+    if (URL === "") {
+        URL = API_BASE_URL + ENDPOINT + API_KEYS[0] + PARAMS + "&language=en";
+    }
 
-    for (let i = 0; i < URLS.length; i++) {
-        try {
-            const response = await fetch(URLS[i]);
-            const data = await response.json();
+    try {
+        const response = await fetch(URL);
+        const data = await response.json();
 
-            if (data.status === "error") throw Error(data.message);
+        if (data.status === "error") throw Error(data.message);
 
-            if (data.status === "ok") {
-                setNewsDict({
-                    ...newsDict,
-                    all: data.articles,
-                });
-                setNewsDictBackup({
-                    ...newsDict,
-                    all: data.articles,
-                });
-                console.log(data);
-                break;
-            }
-        } catch (error) {
-            console.log(error.message);
+        if (data.status === "success") {
+            console.log(data);
+            return data.results;
         }
+    } catch (error) {
+        console.log(error.message);
+    }
+    return false;
+}
+
+export async function fetchNewsWithMultipleKeys(
+    PARAMS,
+    URLS = [],
+    API_BASE_URL = "https://newsdata.io/",
+    ENDPOINT = "api/1/news?"
+) {
+    if (URLS.length === 0)
+        URLS = API_KEYS.map(
+            (API_KEY) => API_BASE_URL + ENDPOINT + API_KEY + PARAMS + "&language=en"
+        );
+
+    for (let attempt = 0; attempt < URLS.length; attempt++) {
+        console.log("Fetching news - attempt - " + (attempt + 1));
+        const results = await fetchNews((URL = URLS[attempt]));
+        if (results === false) continue;
+        console.log(results);
+        return results;
     }
 }
