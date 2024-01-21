4f515615cc9b3dec114a15cc07b26c69bcb4d86f
hexadivine <hexadivine@gmail.com>
2024-01-22 03:31:40 +0530

    removed unneccessory comments

--- src/components/Main/HeroSection.jsx
+++ src/components/Main/HeroSection.jsx
@@ -1,54 +1,37 @@
 import React from "react";
 import { useEffect } from "react";
 import { fetchNewsWithMultipleKeys } from "../../modules/fetchNews.modules";
+import { countryCodes } from "../../config/config";
 
-function HeroSection({ setNewsDict, searchBy, newsDict, newsDictBackup, category }) {
-    // useEffect(() => {
-    //     if (newsDict[category] === undefined);
-    // }, [category]);
-
-    // filter(
-    //     (news) =>
-    //         news.title.toLowerCase().includes(searchBy.toLowerCase()) ||
-    //         news.description.toLowerCase().includes(searchBy.toLowerCase())
-    // )
-
-    // useEffect(() => {
-    //     function removeDuplicate() {
-    //         let articleId = {};
-    //         for (let i = 0; i < newsDict.length; i++) {
-    //             if (articleId[newsDict[i].articleId] === undefined)
-    //                 articleId[newsDict[i].article_id] = true;
-    //             else {
-    //                 newsDict.splice(i, 0);
-    //             }
-    //         }
-    //     }
-    // }, [newsDict]);
-
+function HeroSection({ setNewsDict, searchBy, newsDict, category, country }) {
     useEffect(() => {
-        if (category?.toLowerCase() === "all") return;
+        console.log(newsDict);
+        if (newsDict.length !== 0)
+            fetchNewsWithMultipleKeys(
+                `${category?.toLowerCase() !== "all" ? "&category=" + category.toLowerCase() : ""}${country.toLowerCase() !== "global" ? "&country=" + countryCodes[country] : ""}`
+            ).then((response) => {
+                if (response === undefined) return;
+                setNewsDict((prevNews) => [...new Set([...response, ...prevNews])]);
+            });
+    }, [category, country]);
 
-        fetchNewsWithMultipleKeys(`&category=${category?.toLowerCase()}`).then((response) => {
-            if (response === undefined) return;
-            setNewsDict((prevNews) => [...new Set(response), ...prevNews]);
-        });
-    }, [category]);
-
-    console.log(newsDict);
-    console.log(category);
+    // console.log(newsDict);
+    // console.log(category);
     return (
         <div className="transition-all duration-1000 ease-in-out relative h-[calc(100vh-115px)] rounded-t-[20px] mx-[30px] bg-[rgba(88,88,88,0.19)] p-[20px]">
             <div className="transition-all duration-1000 ease-in-out h-full grid grid-flow-dense grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-[1.5em] mx-auto pt-[20px] overflow-y-scroll no-scrollbar ">
-                {/* {JSON.stringify(newsDict)} */}
-                {/* {JSON.stringify(newsDict.map((k) => JSON.stringify(k.title)))} */}
-
                 {newsDict
+                    ?.filter((news) =>
+                        country?.toLowerCase() === "global"
+                            ? true
+                            : news.country.includes(country.toLowerCase())
+                    )
                     ?.filter((news) => news?.title?.toLowerCase().includes(searchBy.toLowerCase()))
-                    ?.filter((news) => {
-                        if (category.toLowerCase() === "all") return true;
-                        else return news.category?.includes(category.toLowerCase());
-                    })
+                    ?.filter((news) =>
+                        category.toLowerCase() === "all"
+                            ? true
+                            : news.category?.includes(category.toLowerCase())
+                    )
                     .map((news, index) => (
                         <div
                             key={index}
@@ -72,13 +55,22 @@ function HeroSection({ setNewsDict, searchBy, newsDict, newsDictBackup, category
                                 backgroundSize: "cover",
                                 backgroundColor: `${news?.image_url === "" || news?.image_url === null ? "#58585830" : "#390b2686"}`,
                                 backgroundBlendMode: "multiply",
-                                // boxShadow: "0 -150px 200px inset black",
-                                // gridTemplateColumns: 'repeat("auto-fit", minmax("300px", "1fr"))',
                             }}
                         >
+                            {/* <div className="flags">
+                                {news?.country?.map((country_name, index) => (
+                                    <img
+                                        src={`url(https://flagsapi.com/${countryCodes[Object.keys(countryCodes).find((value) => value.toLowerCase() === country_name.toLowerCase())]}/flat/64.png)`}
+                                        className="size-10"
+                                        alt={`${country_name} - ${Object.keys(countryCodes).find((value) => value.toLowerCase() === country_name.toLowerCase())}`}
+                                        key={index}
+                                    />
+                                ))}
+                            </div> */}
                             <b className="text-[20px] text-white opacity-100 mb-[10px]">
-                                {news?.title} - {index}{" "}
+                                {news?.title}
                             </b>
+                            <p className="text-[gray]">{news?.pubDate}</p>
                             <p className="text-[gray]">{news?.description?.slice(0, 300)}...</p>
                         </div>
                     ))}
